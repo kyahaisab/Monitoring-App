df696dd0cee8a5c5148f5e73649946b7627cfcbe
shivsagarsingh <shiv7.singh@paytm.com>
2022-02-12 15:07:36 +0530

    Added battry charging status and improved recycler view

--- app/src/main/java/com/example/notesapp/worker/MyWorkerService.kt
+++ app/src/main/java/com/example/notesapp/worker/MyWorkerService.kt
@@ -1,6 +1,9 @@
 package com.example.notesapp.worker
 
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.util.Log
 import androidx.work.*
 import com.example.notesapp.Note
@@ -40,6 +43,7 @@ class MyWorkerService(context: Context, workerParams: WorkerParameters) :
         val fromMainActivity = inputData.getString(PARSE_CONSTANT)
         val ramUsage = getUsedMemorySize().toString()
         val currTime = Calendar.getInstance().time.toString()
+        val chargingStatus = isCharging()
         val data = Data.Builder().putString(PARSE_CONSTANT, "$currTime $ramUsage").build()
 
         //Because of inserting Toast message thing are not working
@@ -47,11 +51,26 @@ class MyWorkerService(context: Context, workerParams: WorkerParameters) :
         Log.e("SAGAR", "Visited Worker $ramUsage      $currTime")
 
         CoroutineScope(Dispatchers.Main).launch {
-            repository.insert(Note(currTime+"/n"+ramUsage))
+            repository.insert(Note("->TIME: $currTime\n->RAM RAM: $ramUsage\n->CHARGING STATE: $chargingStatus"))
         }
 
         AlarmInfo.setAlert(applicationContext)
         return Result.success(data)
+    }
+
+    private fun isCharging(): Any {
+        val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+            applicationContext.registerReceiver(null, ifilter)
+        }
+            val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+            val isCharging: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING
+                    || status == BatteryManager.BATTERY_STATUS_FULL
+
+        val chargePlug: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) ?: -1
+        val usbCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
+        val acCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
+        val ans = "Is Charging: $isCharging\n->USB POWER: $usbCharge\n->AC POWER: $acCharge"
+        return ans
     }
 
     private fun getUsedMemorySize(): Long {
